547aaa5fa778b0113c929d6d5a44b044ec0e0c7a
TakehitoSato <40849423+TakehitoSato@users.noreply.github.com>
2018-12-13 15:16:56 +0900

    Add files via upload

--- test20181129.py
+++ test20181129.py
@@ -8,32 +8,33 @@ from dwave.system.composites import EmbeddingComposite
 #R:return rate
 #Rcov:covariance of R
 #N:number of assets
+#T:number of time
 #theta:weight
 #->1:return 2:risk aversion 3:constrain
 
+#setting parameters
+T = 6
 N = 10
-B = 200
-theta1 = 0.1
-theta2 = 0.1
-theta3 = 0.8
+B = 150
+theta1 = 0.3
+theta2 = 0.3
+theta3 = 3.0
 
 np.random.seed(5)
-P = np.random.randint(1,100,(N))
-#Pavg = np.average(P)
-P_2 = np.random.randint(1,100,(N))
-#P_2avg = np.average(P_2)
 
-Rcov = np.empty((N,N))
-R = np.empty(N)
-#calculate expected return rate
-for i in range(N):
-    R[i] = (P_2[i] - P[i]) / P[i]
-Ravg = np.average(R)
+#make random prices
+P = np.random.randint(1,100,(T,N))
 
-#calculate covariance(and variance)
-for i in range(N):
-    for j in range(N):
-        Rcov[i,j] = ( (R[i] - Ravg)*(R[j] - Ravg) ) / N
+#calculate return rate
+R = np.zeros((T-1,N))
+for j in range(N):
+    for i in range(T-1):
+        R[i,j] = (P[i+1,j] -P[i,j]) / P[i,j]
+
+#calculate covariance of R        
+#axis = 0:calculate over column,bias = 1:normalization by N
+Ravg = np.average(R, axis=0)
+Rcov = np.cov(R, rowvar=0, bias=1)
 
 #make QUBO
 Qmirror = np.empty((N,N))
@@ -41,26 +42,23 @@ Q = {}
 for i in range(N):
     for j in range(N):
         if i == j:
-            Qmirror[i,j] =  ( theta2*Rcov[i,j] + theta3*P[i]*P[i] - theta1*R[i] -2*theta3*B*P[i] )
+            Qmirror[i,j] =  ( theta2*Rcov[i,j] + theta3*P[-1,i]*P[-1,i] - theta1*Ravg[i] -2*theta3*B*P[-1,i] ) / 2
             Q[i,j] = Qmirror[i,j]
         else:
-            Qmirror[i,j] =  ( theta2*Rcov[i,j] + theta3*P[i]*P[j] )
+            Qmirror[i,j] =  ( theta2*Rcov[i,j] + theta3*P[-1,i]*P[-1,j] ) / 4
             Q[i,j] = Qmirror[i,j]
 
 #sampling with D-wave machine           
 sampler = EmbeddingComposite(DWaveSampler())
-response = sampler.sample_qubo(Q, num_reads=1000)
+response = sampler.sample_qubo(Q, num_reads=100)
 for sample in response.samples():
     print(sample)
 
-
+#check sum
 sum = 0
-returnsum = 0
 for i in range(N):
     if response.record['sample'][0,i] == 1:
-        sum += P[i]
-        returnsum += P[i] * R[i]
+        sum += P[-1,i]
 
 print(response.record['sample'][0])        
 print(sum)
-print(returnsum)
